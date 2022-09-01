f2fc41e86ec0649b0e3c2dfc104f623bb6cee1ed
Spy-sathivk <85746840+Spy-sathivk@users.noreply.github.com>
2022-09-01 20:14:34 +0530

    Add files via upload

---
+++ projectc110.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+import time
+from watchdog.observers import observer
+import random
+
+randint(start,end)
+no = random.randint(1,6)
+if value ==random
+ print('press y to roll again')
+ 
